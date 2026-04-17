be8e85101775dc5e989dff0358fa6fa9e5bd523f
Omar Mohamed <65883722+Omar-MK-20@users.noreply.github.com>
2026-04-17 02:03:42 +0200

    Refactor token management: Implement JWT token generation and verification with improved error handling and role-based signature retrieval

--- src/util/security/token.ts
+++ src/util/security/token.ts
@@ -1,105 +1,81 @@
-// import jwt from "jsonwebtoken";
-// import { TokenType } from "../enums/token.enums.js";
-// import { UserRole } from "../enums/user.enums.js";
-// import { TOKEN_SIGNATURE_ADMIN_ACCESS, TOKEN_SIGNATURE_ADMIN_REFRESH, TOKEN_SIGNATURE_USER_ACCESS, TOKEN_SIGNATURE_USER_REFRESH } from "../../app.config.js";
+import jwt, { JsonWebTokenError, TokenExpiredError } from "jsonwebtoken";
+import { TOKEN_SIGNATURE_ADMIN_ACCESS, TOKEN_SIGNATURE_ADMIN_REFRESH, TOKEN_SIGNATURE_USER_ACCESS, TOKEN_SIGNATURE_USER_REFRESH } from "../../app.config.js";
+import { AuthType, TokenType } from "../enums/token.enums.js";
+import { UserRole } from "../enums/user.enums.js";
+import type { ITokenPayload } from "../interfaces/ITokenPayload.js";
+import { ContentError, UnauthorizedError } from "../res/ResponseError.js";
 
 
-// export function tokenGenerator(userData: { id: string, email: string, role: string; }, tokenType: string)
-// {
-//     const { refreshSignature, accessSignature } = getSignature(userData.role);
+export function tokenGenerator(userData: { id: string, email: string, role: UserRole; }, tokenType: string)
+{
+    const { refreshSignature, accessSignature } = getSignature(userData.role);
 
-//     const signature = tokenType == TokenType.access ? accessSignature : refreshSignature;
-//     const expiresIn = tokenType == TokenType.access ? "10m" : "1y";
+    const signature = tokenType == TokenType.access ? accessSignature : refreshSignature;
+    const expiresIn = tokenType == TokenType.access ? "10m" : "1y";
 
-//     const token = jwt.sign({ ...userData, tokenType }, signature, { expiresIn: expiresIn });
+    const token = jwt.sign({ ...userData, tokenType }, signature, { expiresIn: expiresIn });
 
-//     return token;
-// }
-
-
-// /**
-//  * 
-//  * @param {string} authorization The JsonWebToken string
-//  * @param {string} desiredTokenType The desired token type to check the validity of token upon it.
-//  * @returns {{
-//  * header: { alg: string, typ: string }, 
-//  * payload: {id: string, email: string, iat: string, exp: string}, 
-//  * signature: string} 
-//  * | null}
-//  */
-// export function verifyToken(desiredAuthType, authorization, desiredTokenType)
-// {
-//     try
-//     {
-//         const [authType, token] = authorization.split(" ");
-
-//         if (desiredAuthType != authType)
-//         {
-//             throw new UnauthorizedError({ message: "you are not authorized", info: { authorization } });
-//         }
-
-//         const decoded = jwt.decode(token);
-
-//         const { accessSignature, refreshSignature } = getSignature(decoded.role);
-
-//         const signature = desiredTokenType == TokenType.access ? accessSignature : refreshSignature;
-
-//         const data = jwt.verify(token, signature, { complete: true });
-//         return data;
-//     }
-//     catch (error)
-//     {
-//         if (error.name === "TokenExpiredError")
-//         {
-//             throw new UnauthorizedError({ message: "token expired", info: { error } });
-//         }
-//         if (error.name === "JsonWebTokenError")
-//         {
-//             throw new ContentError({ message: error.message, info: { error } });
-//         }
-
-//         throw error;
-//     }
-// }
+    return token;
+}
 
 
-// // const token = tokenGenerator({ id: 1234, name: "omar", email: "omar@email.com" });
-// // console.log(token);
+export function verifyToken(desiredAuthType: AuthType, authorization: string, desiredTokenType: TokenType)
+{
+    try
+    {
+        const [authType, token] = authorization.split(" ");
 
-// // const data = checkToken(token);
-// // console.log(data);
+        if (desiredAuthType != authType)
+        {
+            throw new UnauthorizedError({ message: "you are not authorized", info: { authorization } });
+        }
+
+        if (!token) throw new ContentError({ message: "token is required" });
+
+        const decoded = jwt.decode(token) as ITokenPayload;
+
+        const { accessSignature, refreshSignature } = getSignature(decoded.role);
+
+        const signature = desiredTokenType == TokenType.access ? accessSignature : refreshSignature;
+
+        const data = jwt.verify(token, signature, { complete: true });
+        return data;
+    }
+    catch (error) 
+    {
+        if (error instanceof TokenExpiredError)
+        {
+            throw new UnauthorizedError({ message: "token expired", info: { error } });
+        }
+
+        if (error instanceof JsonWebTokenError)
+        {
+            throw new ContentError({ message: error.message, info: { error } });
+        }
+
+        throw error;
+    }
+}
 
 
 
-// export function getSignature(userRole: UserRole)
-// {
-//     let refreshSignature;
-//     let accessSignature;
+export function getSignature(userRole: UserRole)
+{
+    let refreshSignature: Buffer;
+    let accessSignature: Buffer;
 
-//     switch (undefined)
-//     {
-//         case TOKEN_SIGNATURE_ADMIN_ACCESS:
-//         case TOKEN_SIGNATURE_ADMIN_REFRESH:
-//         case TOKEN_SIGNATURE_USER_ACCESS:
-//         case TOKEN_SIGNATURE_USER_REFRESH:
-//             break;
-//     }
+    switch (userRole)
+    {
+        case UserRole.admin:
+            accessSignature = Buffer.from(TOKEN_SIGNATURE_ADMIN_ACCESS || "", "hex");
+            refreshSignature = Buffer.from(TOKEN_SIGNATURE_ADMIN_REFRESH || "", "hex");
+            break;
 
-//     switch (userRole)
-//     {
-//         case UserRole.admin:
-//             accessSignature = Buffer.from(TOKEN_SIGNATURE_ADMIN_ACCESS, "hex");
-//             refreshSignature = Buffer.from(TOKEN_SIGNATURE_ADMIN_REFRESH, "hex");
-//             break;
+        case UserRole.user:
+            accessSignature = Buffer.from(TOKEN_SIGNATURE_USER_ACCESS || "", "hex");
+            refreshSignature = Buffer.from(TOKEN_SIGNATURE_USER_REFRESH || "", "hex");
+            break;
+    }
 
-//         case UserRole.user:
-//             accessSignature = Buffer.from(TOKEN_SIGNATURE_USER_ACCESS, "hex");
-//             refreshSignature = Buffer.from(TOKEN_SIGNATURE_USER_REFRESH, "hex");
-//             break;
-
-//         default:
-//             break;
-//     }
-
-//     return { refreshSignature, accessSignature };
-// }
+    return { refreshSignature, accessSignature };
+}
