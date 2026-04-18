5489b476248112069660dc908495c1ba4e19ddcc
Omar Mohamed <65883722+Omar-MK-20@users.noreply.github.com>
2026-04-19 01:58:00 +0200

    Enhance user model: Introduce IUser interface with additional fields and update userSchema to include gender, DOB, role, provider, and other properties for improved user data management

--- src/DB/Models/user.model.ts
+++ src/DB/Models/user.model.ts
@@ -1,7 +1,28 @@
+import type { HydratedDocument } from 'mongoose';
 import { model, Schema } from "mongoose";
+import { GenderEnum, ProviderEnum, RoleEnum } from "../../util/enums/user.enums.js";
+
+export interface IUser
+{
+    username: string;
+    email: string;
+    password: string;
+    DOB?: Date | undefined;
+    gender?: GenderEnum | undefined;
+    confirmEmail?: boolean | undefined;
+    phone?: string | undefined;
+    role?: RoleEnum | undefined;
+    provider: ProviderEnum;
+    profilePic?: string | undefined;
+    coverPics?: string[] | undefined;
+    changeCreditTime: Date;
+}
 
 
-const userSchema = new Schema({
+export type HUser = HydratedDocument<IUser>;
+
+
+const userSchema = new Schema<IUser>({
     username: {
         type: String,
         required: true,
@@ -13,10 +34,39 @@ const userSchema = new Schema({
     },
     password: {
         type: String,
-        required: true,
+        required: function (): boolean { return this.provider == ProviderEnum.System; },
         select: false,
     },
+    DOB: Date,
+    gender: {
+        type: Number,
+        enum: GenderEnum,
+        default: GenderEnum.Male,
+        required: false,
+    },
+    confirmEmail: {
+        type: Boolean,
+        default: false,
+        required: false,
+    },
+    phone: {
+        type: String,
+    },
+    role: {
+        type: Number,
+        enum: RoleEnum,
+        default: RoleEnum.User,
+    },
+    profilePic: String,
+    coverPics: [String],
+    provider: {
+        type: Number,
+        enum: ProviderEnum,
+        default: ProviderEnum.System
+    },
+    changeCreditTime: Date
+
 });
 
 
-export const UserModel = model("users", userSchema);
+export const UserModel = model<IUser>("users", userSchema);
