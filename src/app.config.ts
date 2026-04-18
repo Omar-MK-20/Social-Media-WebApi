0d4cbb7b9a1750ed4310139d3d8823733bd459e5
Omar Mohamed <65883722+Omar-MK-20@users.noreply.github.com>
2026-04-19 01:57:27 +0200

    Update app configuration: Cast environment variables to string type for improved type safety and add ALGORITHM constant

--- src/app.config.ts
+++ src/app.config.ts
@@ -4,14 +4,15 @@ import path from 'node:path';
 
 dotenv.config({ path: path.resolve("./config/.env.dev") });
 
-export const SEVER_PORT = process.env.SEVER_PORT;
-export const DB_URI = process.env.DB_URI;
-export const DB_URI_FALLBACK = process.env.DB_URI_FALLBACK;
-export const ENCRYPTION_SECRET_KEY = process.env.ENCRYPTION_SECRET_KEY;
-export const TOKEN_SIGNATURE_ADMIN_ACCESS = process.env.TOKEN_SIGNATURE_ADMIN_ACCESS;
-export const TOKEN_SIGNATURE_USER_ACCESS = process.env.TOKEN_SIGNATURE_USER_ACCESS;
-export const TOKEN_SIGNATURE_ADMIN_REFRESH = process.env.TOKEN_SIGNATURE_ADMIN_REFRESH;
-export const TOKEN_SIGNATURE_USER_REFRESH = process.env.TOKEN_SIGNATURE_USER_REFRESH;
-// export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+export const SEVER_PORT = process.env.SEVER_PORT as string;
+export const DB_URI = process.env.DB_URI as string;
+export const DB_URI_FALLBACK = process.env.DB_URI_FALLBACK as string;
+export const ENCRYPTION_SECRET_KEY = process.env.ENCRYPTION_SECRET_KEY as string;
+export const TOKEN_SIGNATURE_ADMIN_ACCESS = process.env.TOKEN_SIGNATURE_ADMIN_ACCESS as string;
+export const TOKEN_SIGNATURE_USER_ACCESS = process.env.TOKEN_SIGNATURE_USER_ACCESS as string;
+export const TOKEN_SIGNATURE_ADMIN_REFRESH = process.env.TOKEN_SIGNATURE_ADMIN_REFRESH as string;
+export const TOKEN_SIGNATURE_USER_REFRESH = process.env.TOKEN_SIGNATURE_USER_REFRESH as string;
+export const ALGORITHM = process.env.ALGORITHM as string;
+// export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID  as string;
 // export const GOOGLE_NODEMAILER_USER = process.env.GOOGLE_NODEMAILER_USER;
 // export const GOOGLE_APP_PASSWORD = process.env.GOOGLE_APP_PASSWORD;
