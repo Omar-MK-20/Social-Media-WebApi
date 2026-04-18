1510e1a11915eefc0d444e1a2293a75995377ec7
Omar Mohamed <65883722+Omar-MK-20@users.noreply.github.com>
2026-04-19 01:59:14 +0200

    Implement validation middleware for signup route: Integrate validation schema to enhance input validation during user signup process.

--- src/Modules/Auth/auth.controller.ts
+++ src/Modules/Auth/auth.controller.ts
@@ -1,14 +1,18 @@
 import { Router } from "express";
-import { createSuccessObject, successResponse } from "../../util/res/ResponseObject.js";
+import { successResponse } from "../../util/res/ResponseObject.js";
 import * as authService from "./auth.service.js";
+import { validation } from "../../util/middlewares/validation.middleware.js";
+import { signupSchema } from "./auth.validation.js";
 
 
 
 export const authRouter = Router();
 
-authRouter.post("/signup", async (req, res) =>
-{
-    const result = await authService.signup(req.body);
+authRouter.post("/signup",
+    validation(signupSchema),
+    async (req, res) =>
+    {
+        const result = await authService.signup(req.body);
 
-    successResponse(res, result);
-});
+        successResponse(res, result);
+    });
