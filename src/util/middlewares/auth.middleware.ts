f1b71816b7b695d69f7b31100d0d32d25e3bce59
Omar-MK-20 <omar.mk.201@gmail.com>
2026-04-19 18:09:45 +0200

    feat: add authentication and authorization middleware for user access control

---
+++ src/util/middlewares/auth.middleware.ts
@@ -0,0 +1,47 @@
+import type { Request, Response, NextFunction } from "express";
+import { AuthType, TokenType } from "../enums/token.enums.js";
+import { ContentError, ForbiddenError, UnauthorizedError } from "../res/ResponseError.js";
+import { verifyToken } from "../security/token.js";
+import userRepo from "../../DB/Repos/user.repo.js";
+import type { RoleEnum } from "../enums/user.enums.js";
+
+
+export function authentication(tokenType = TokenType.access, authType = AuthType.bearer)
+{
+    return async (req: Request, res: Response, next: NextFunction) =>
+    {
+        const { authorization } = req.headers;
+
+        if (!authorization)
+        {
+            throw new ContentError({ message: "token is required", info: { authorization } });
+        }
+
+        const payload = verifyToken(authType, authorization, tokenType);
+
+        if (typeof payload == "string") { throw new ContentError({ message: "Invalid Token data", info: { payload } }); }
+
+        const result = await userRepo.findById(payload.id);
+        if (!result)
+        {
+            throw new UnauthorizedError({ message: "user not found, signup first" });
+        }
+
+        req.user = result;
+        next();
+    };
+}
+
+
+export function authorization(...roles: RoleEnum[])
+{
+    return (req: Request, res: Response, next: NextFunction) =>
+    {
+        if (!roles.includes(req.user.role))
+        {
+            throw new ForbiddenError({ message: "You don't have access to this API", info: { role: req.user.role } });
+        }
+
+        next();
+    };
+}
