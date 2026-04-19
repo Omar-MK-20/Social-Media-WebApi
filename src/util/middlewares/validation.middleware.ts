9c2291b654b6aa75750f9e67ccd5d6b693c6e8d5
Omar Mohamed <65883722+Omar-MK-20@users.noreply.github.com>
2026-04-19 02:02:27 +0200

    Add validation middleware: Introduce validation.middleware.ts to implement input validation using Zod schema for various user fields, enhancing data integrity and type safety.

---
+++ src/util/middlewares/validation.middleware.ts
@@ -0,0 +1,54 @@
+import type { NextFunction, Request, Response } from "express";
+import z, { ZodObject } from "zod";
+import type { $ZodIssue } from "zod/v4/core";
+import { GenderEnum, RoleEnum } from "../enums/user.enums.js";
+import { ContentError } from "../res/ResponseError.js";
+
+
+export type ReqKey = keyof Request;
+
+export function validation(validationSchema: Partial<Record<ReqKey, ZodObject>>)
+{
+    return (req: Request, res: Response, next: NextFunction) =>
+    {
+        let validationError: $ZodIssue[] = [];
+
+        for (const key in validationSchema)
+        {
+            // TODO solve `as ReqKey`
+            const result = validationSchema[key as ReqKey]!.safeParse(req[key as ReqKey]);
+            if (!result.success)
+            {
+                validationError.push(...result.error.issues);
+            }
+        }
+
+        if (!validationError.length)
+        {
+            throw new ContentError({ message: "Invalid Data", info: validationError });
+        }
+
+        next();
+    };
+}
+
+
+
+
+const passwordRegExp = /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,16}$/;
+const phoneRegExp = /^01[0125][0-9]{8}$/;
+
+
+export const ValidationType = {
+    username: z.string().min(3).max(50),
+    email: z.email(),
+    password: z.string().regex(passwordRegExp),
+    DOB: z.date(),
+    gender: z.enum(GenderEnum).default(GenderEnum.Male),
+    confirmEmail: z.boolean().default(false),
+    phone: z.string().regex(phoneRegExp),
+    role: z.enum(RoleEnum).default(RoleEnum.User),
+    profilePic: z.string(),
+    coverPics: z.array(z.string()),
+
+};
