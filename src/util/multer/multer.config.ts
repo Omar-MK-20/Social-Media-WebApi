2fdd29c922209d2dc900aacaf590a09535743843
Omar-MK-20 <omar.mk.201@gmail.com>
2026-05-14 18:46:12 +0300

    feat: enhance uploadFile function with allowedFormats and storageType options

--- src/util/multer/multer.config.ts
+++ src/util/multer/multer.config.ts
@@ -1,20 +1,44 @@
 import type { Request, } from "express";
 import multer, { type FileFilterCallback } from "multer";
+import { randomUUID } from "node:crypto";
+import { tmpdir } from "node:os";
+import { FileFormats, StorageType, type MimeType } from "../enums/file.enums.js";
+import { ContentError } from "../res/ResponseError.js";
 
 
-export function uploadFile(uploadOptions: { fileSize?: number; filesCount?: number; } = {})
+
+export function uploadFile(uploadOptions: { allowedFormats?: MimeType[]; storageType?: StorageType; fileSize?: number; filesCount?: number; } = {})
 {
-    const { fileSize = 5, filesCount = 1 } = uploadOptions;
+    const { allowedFormats = FileFormats.image, storageType = StorageType.Memory, fileSize = 5, filesCount = 1 } = uploadOptions;
 
-    const storage = multer.memoryStorage();
 
-    // function fileFilter(req: Request, file: Express.Multer.File, callback: FileFilterCallback)
-    // {
 
-    // }
+    const storage = storageType == StorageType.Memory
+        ? multer.memoryStorage()
+        : multer.diskStorage({
+            destination(req, file, callback)
+            {
+                callback(null, tmpdir());
+            },
+            filename(req, file, callback)
+            {
+                callback(null, `${randomUUID()}_${file.originalname.split(" ").join("-")}`);
+            },
+        });
 
-    const multerOptions: multer.Options = { storage };
 
+
+    function fileFilter(req: Request, file: Express.Multer.File, callback: FileFilterCallback)
+    {
+        if (!allowedFormats.includes(file.mimetype as MimeType))
+        {
+            return callback(new ContentError({ message: "Invalid File format", info: { "allowed file formats": allowedFormats } }));
+        }
+        return callback(null, true);
+    }
+
+
+    const multerOptions: multer.Options = { storage, fileFilter };
     multerOptions.limits = {
         fileSize: (fileSize * 1024 * 1024),
         files: filesCount
