c978f60cc903d227fb9e110244ccda93aff9ef86
Omar-MK-20 <omar.mk.201@gmail.com>
2026-05-19 14:03:40 +0300

    feat: update validation middleware to support ZodArray in validation schema and convert ValidationType to static class

--- src/util/middlewares/validation.middleware.ts
+++ src/util/middlewares/validation.middleware.ts
@@ -1,5 +1,5 @@
 import type { NextFunction, Request, Response } from "express";
-import z, { ZodObject } from "zod";
+import z, { ZodArray, ZodObject } from "zod";
 import type { $ZodIssue } from "zod/v4/core";
 import { MIME_TYPES, StorageType } from "../enums/file.enums.js";
 import { GenderEnum, RoleEnum } from "../enums/user.enums.js";
@@ -7,7 +7,7 @@ import { ContentError } from "../res/ResponseError.js";
 
 export type ReqKey = keyof Request;
 
-export function validation(validationSchema: Partial<Record<ReqKey, ZodObject>>)
+export function validation(validationSchema: Partial<Record<ReqKey, ZodObject | ZodArray>>)
 {
     return (req: Request, res: Response, next: NextFunction) =>
     {
@@ -42,31 +42,34 @@ const passwordRegExp = /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8
 const phoneRegExp = /^01[0125][0-9]{8}$/;
 
 
-export const ValidationType = {
-    id: z.string({ error: "id is required" })
-        .regex(/^[0-9a-fA-F]{24}$/, { error: "invalid id" }),
-    id2: z.hex().max(24).min(24), // for testing
-    username: z.string().min(3).max(50),
-    email: z.email(),
-    password: z.string().regex(passwordRegExp),
-    DOB: z.date(),
-    gender: z.enum(GenderEnum).default(GenderEnum.Male),
-    confirmEmail: z.boolean().default(false),
-    phone: z.string().regex(phoneRegExp),
-    role: z.enum(RoleEnum).default(RoleEnum.User),
-    profilePic: z.string(),
-    coverPics: z.array(z.string()),
-    fromAllDevices: z.boolean(),
-    otp: z.number().min(100000).max(999999),
-    memoryFile: z.object({
+export class ValidationType
+{
+    private constructor() { }
+    static id = z
+        .string({ error: "id is required" })
+        .regex(/^[0-9a-fA-F]{24}$/, { error: "invalid id" });
+    static id2 = z.hex().max(24).min(24);
+    static username = z.string().min(3).max(50);
+    static email = z.email();
+    static password = z.string().regex(passwordRegExp);
+    static DOB = z.date();
+    static gender = z.enum(GenderEnum).default(GenderEnum.Male);
+    static confirmEmail = z.boolean().default(false);
+    static phone = z.string().regex(phoneRegExp);
+    static role = z.enum(RoleEnum).default(RoleEnum.User);
+    static profilePic = z.string();
+    static coverPics = z.array(z.string());
+    static fromAllDevices = z.boolean();
+    static otp = z.number().min(100000).max(999999);
+    private static memoryFile = z.object({
         fieldname: z.string(),
         originalname: z.string(),
         encoding: z.string(),
         mimetype: z.enum(MIME_TYPES),
         buffer: z.instanceof(Buffer),
         size: z.number(),
-    }),
-    diskFile: z.object({
+    });
+    private static diskFile = z.object({
         fieldname: z.string(),
         originalname: z.string(),
         encoding: z.string(),
@@ -75,11 +78,11 @@ export const ValidationType = {
         filename: z.string(),
         path: z.string(),
         size: z.number(),
-    }),
-    file(storageType: StorageType)
+    });
+    static file(storageType: StorageType)
     {
-        return storageType == StorageType.Memory
+        return storageType === StorageType.Memory
             ? this.memoryFile
             : this.diskFile;
-    },
-};
+    }
+}
